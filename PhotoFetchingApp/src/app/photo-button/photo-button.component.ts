cbcdf1221724d7e9e1825144e1aa8f8c3c547da5
Nicole Candiotti <nccandiotti@gmail.com>
2022-06-24 14:57:12 -0400

    photo fetching app completed

--- PhotoFetchingApp/src/app/photo-button/photo-button.component.ts
+++ PhotoFetchingApp/src/app/photo-button/photo-button.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-
+import { PhotoFetchService } from '../photo-fetch.service';
 @Component({
   selector: 'app-photo-button',
   templateUrl: './photo-button.component.html',
-  styleUrls: ['./photo-button.component.css']
+  styleUrls: ['./photo-button.component.css'],
 })
 export class PhotoButtonComponent implements OnInit {
+  showPhoto: boolean = false;
+  photoUrl: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private photoFetchService: PhotoFetchService) {
+    this.fetchPhoto();
   }
 
+  handleClick() {
+    this.fetchPhoto();
+    this.showPhoto = true;
+  }
+
+  fetchPhoto() {
+    this.photoFetchService.getPhoto().subscribe((response) => {
+      this.photoUrl = response.urls.regular;
+      console.log(response.urls.regular);
+    });
+  }
+
+  ngOnInit(): void {}
 }
--- PhotoFetchingApp/src/app/photo-fetch.service.ts
+++ PhotoFetchingApp/src/app/photo-fetch.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface UnsplashResponse {
+  urls: {
+    regular: string;
+  };
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoFetchService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getPhoto() {
+    return this.http.get<UnsplashResponse>(
+      'https://api.unsplash.com/photos/random',
+      {
+        headers: {
+          Authorization:
+            'Client-ID RD7jIBucmQT6gRW7i6cFknvtleoAcuN4VeyLK__EAyA',
+        },
+      }
+    );
+  }
 }
