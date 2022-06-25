c3953e0fc3ae4a2799af2986dc3ebc728620c893
Nicole Candiotti <nccandiotti@gmail.com>
2022-06-25 18:02:21 -0400

    validators and refactored input to own component

--- creditCard/src/app/card-form/card-form.component.ts
+++ creditCard/src/app/card-form/card-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// form group tells angular about the form we are creating, FormControl is specifically about each of the fields
-import { FormGroup, FormControl } from '@angular/forms';
+// form group tells angular about the form we are creating, FormControl is specifically for each of the fields in the form
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-card-form',
@@ -8,6 +8,16 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./card-form.component.css'],
 })
 export class CardFormComponent implements OnInit {
+  // instance of the form, pass in an object with key/value pairs that are the form fields
+  // FormControl - each field. first argument is the default value, second arg is an array of validator(s) (if using validators)
+  cardForm = new FormGroup({
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(5),
+      // Validators.pattern(/\s/),
+    ]),
+  });
   constructor() {}
 
   ngOnInit(): void {}
