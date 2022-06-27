68e9bd51a3b6a25fb1ef8dc2e351242a14990f1d
Nicole Candiotti <nccandiotti@gmail.com>
2022-06-27 15:27:11 -0400

    JavaShop project edits, service created for coffees, drink form styled, bulma installed

--- JavaShopRedoAngular/src/app/components/cart/cart.component.ts
+++ JavaShopRedoAngular/src/app/components/cart/cart.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
+  cartTotal: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
