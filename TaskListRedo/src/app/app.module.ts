a8ec3805a8b97cc2566ef2dffc8948288ae3a783
Nicole Candiotti <nccandiotti@gmail.com>
2022-07-02 20:26:50 -0400

    created get request with http client in service

--- TaskListRedo/src/app/app.module.ts
+++ TaskListRedo/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+//http client module
+import { HttpClientModule } from '@angular/common/http';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+//components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,13 +16,10 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
   ],
-  imports: [
-    BrowserModule,
-    FontAwesomeModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- TaskListRedo/src/app/services/task.service.ts
+++ TaskListRedo/src/app/services/task.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
 // import mock json
-import { TASKS } from '../mock-tasks';
+// import { TASKS } from '../mock-tasks';
 // importing interface
 import { Task } from '../task';
 // import observable /rxjs
 
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+
+// http
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
 export class TaskService {
-  constructor() {}
+  private apiUrl = 'http://localhost:3000/tasks';
+
+  constructor(private http: HttpClient) {}
 
   getTasks(): Observable<Task[]> {
-    const tasks = of(TASKS);
-    return tasks;
+    // calling get method/req on http, after "get" specify Task array
+    return this.http.get<Task[]>(this.apiUrl);
   }
 }
