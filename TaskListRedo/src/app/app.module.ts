0563482b360834123b7eb56992adb082354c471e
Nicole Candiotti <nccandiotti@gmail.com>
2022-07-03 13:20:07 -0400

    added routing, app complete

--- TaskListRedo/src/app/app.module.ts
+++ TaskListRedo/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 //http client module
 import { HttpClientModule } from '@angular/common/http';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -11,7 +12,13 @@ import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
 
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,8 +27,16 @@ import { AddTaskComponent } from './components/add-task/add-task.component';
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
   ],
-  imports: [BrowserModule, FontAwesomeModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- TaskListRedo/src/app/components/header/header.component.ts
+++ TaskListRedo/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,11 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task List';
-  constructor() {}
+  showAddTask: boolean = false;
+  subscription: Subscription;
+
+  // reminder - in order to use a service you have to add it as an arg to the constructor
+  constructor(private uiService: UiService, private router: Router) {
+    this.subscription = this.uiService
+      .onToggle()
+      .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {}
 
   toggleAddTask() {
-    console.log('toggle');
+    // this will toggle the boolean value of showAddTask in service
+    this.uiService.toggleAddTask();
+    this.showAddTask;
+  }
+  // returns a string true/false value
+  hasRoute(route: string) {
+    return this.router.url === route;
   }
 }
